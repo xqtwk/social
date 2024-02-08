7d173c23d06dae2116c18d20ba6e282594def99e
Richard <xqtwkk@gmail.com>
2024-02-08 22:10:41 +0300

    routing for showcase

--- frontend/social/src/app/app.routes.ts
+++ frontend/social/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
+import {GroupsComponent} from "./pages/groups/groups.component";
+import {MessagesComponent} from "./pages/messages/messages.component";
+import {FriendsComponent} from "./pages/friends/friends.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'groups', component: GroupsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: 'friends', component: FriendsComponent}
+];
