ff540f63edbc3c3688ff8fd147e7f83ad6935b12
Richard <xqtwkk@gmail.com>
2024-02-08 22:10:22 +0300

    preliminary layout, sidebar implemented

--- frontend/social/src/app/app.component.ts
+++ frontend/social/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {SidebarComponent} from "./pages/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'social';
 }
