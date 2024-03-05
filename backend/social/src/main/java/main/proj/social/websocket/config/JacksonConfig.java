14fe2b43615236b8f934791ce796357698c64ad6
Richard <xqtwkk@gmail.com>
2024-03-05 22:09:40 +0300

    websockets handling implemented

---
+++ backend/social/src/main/java/main/proj/social/websocket/config/JacksonConfig.java
@@ -0,0 +1,19 @@
+package main.proj.social.websocket.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Jdk8Module());
+        return mapper;
+    }
+}
