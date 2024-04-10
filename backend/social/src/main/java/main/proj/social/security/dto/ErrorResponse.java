a4a1d31ade53777b72b6a952f6ce402498e2302a
Richard <xqtwkk@gmail.com>
2024-04-11 00:03:34 +0300

    Removed unused import

--- backend/social/src/main/java/main/proj/social/security/dto/ErrorResponse.java
+++ backend/social/src/main/java/main/proj/social/security/dto/ErrorResponse.java
@@ -2,13 +2,10 @@ package main.proj.social.security.dto;
 
 import lombok.*;
 
-import java.util.Map;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponse {
     private String message;
-
 }
