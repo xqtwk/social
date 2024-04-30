f1031f09be7fd2c1440bc3ddf8d4899d98958e29
Richard <xqtwkk@gmail.com>
2024-04-30 18:53:22 +0300

    generated services&models from api docs using openapi

--- frontend/social/src/app/app.component.ts
+++ frontend/social/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {ApiModule} from "./services/openapi";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, SidebarComponent],
+  imports: [RouterOutlet, RouterLink, SidebarComponent, ApiModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  title = 'social';
 }
